f7b4ac5a0929dde7a904b25c80321acce61cf1b1
YoungjinKim <talkingjuly@gmail.com>
2025-09-09 15:12:21 +0900

    init

---
+++ src/main/java/com/docteory07/divination/IChingApplication.java
@@ -0,0 +1,13 @@
+package com.docteory07.divination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IChingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IChingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docteory07/divination/IChingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docteory07.divination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IChingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
